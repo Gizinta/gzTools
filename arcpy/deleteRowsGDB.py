16a1c5e9768bd8a0cc15d2b749aba35e7a776b80
Steve Grise <support@gizinta.com>
2013-07-16 09:52:22 -0400

    add xml imports
    Added xml.dom.minidom import for tools I don't use with xml parameters
    very often. Significant bug.

--- arcpy/deleteRowsGDB.py
+++ arcpy/deleteRowsGDB.py
@@ -5,7 +5,7 @@
 # Copyright 2012-2013 Vertex3 Inc
 # This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 
-import os, sys, traceback, arcpy, gzSupport
+import os, sys, traceback, xml.dom.minidom, arcpy, gzSupport
 
 # Local variables...
 debug = False
